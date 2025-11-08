508d500b5279433b3c9518f28e7b5fa4fb11fdee
Max Kotlan <max.maxkot@gmail.com>
2025-11-08 12:10:09 -0500

    multiplicatio n maps

--- multiplication_map_generator.py
+++ multiplication_map_generator.py
@@ -1,11 +1,17 @@
+import numpy as np
+from PIL import Image
 
 map_sizes = [4, 8, 16, 32, 64, 128, 256]
 
 for size in map_sizes:
-    for x in range(modrange):
-        for y in range(modrange):
-            q = (x/127) # modified to use 127 instead of 255
-            p = (y/127) # modified to use 127 instead of 255
+    gspace = np.zeros((size, size))
+    for x in range(size):
+        for y in range(size):
+            half=size//2
+            q = (x/half) # modified to use 127 instead of 255
+            p = (y/half) # modified to use 127 instead of 255
             r = q * p
-            g = round(r * 127) + 128 # shift to allow for negative values
+            g = round(r * half) + half # shift to allow for negative values
             gspace[x,y] = g*(256//size)
+
+    Image.fromarray(gspace.astype(np.uint8)).save(f'./multiplication_maps/signed_{size}x{size}.png')
