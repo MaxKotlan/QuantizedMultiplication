4ebd2ba606e5a0a4e403d875000f7298dbeec0a1
Max Kotlan <max.maxkot@gmail.com>
2025-11-09 05:07:01 +0000

    hmm

--- algorithms/nearest_neighbor.py
+++ algorithms/nearest_neighbor.py
@@ -1,16 +1,29 @@
 from multiplication_map_loader import MAP_CONFIG
+import numpy as np
 
-# Keep multiplyIntSpace inside this module, private to nearest neighbor
 def _multiplyIntSpace(a, b, uint8_map):
     size = uint8_map.shape[0]  # assume square
-    x = int(a * (size-1) / 255)
-    y = int(b * (size-1) / 255)
+    x = int(np.clip(a * (size - 1) / 255, 0, size - 1))
+    y = int(np.clip(b * (size - 1) / 255, 0, size - 1))
     return uint8_map[x, y]
 
-def multiplyFloatSpaceNN(fa, fb, uint8_map, map_type='signed'):
+def multiplyFloatSpaceNN(fa, fb, uint8_map, map_type='signed_ext'):
     min_f, max_f = MAP_CONFIG[map_type]['float_range']
     scale = 255 / (max_f - min_f)
-    ia = round((fa - min_f) * scale)
-    ib = round((fb - min_f) * scale)
+
+    # Map floats to uint8 input indices
+    ia = int(np.clip(round((fa - min_f) * scale), 0, 255))
+    ib = int(np.clip(round((fb - min_f) * scale), 0, 255))
+    
     ir = _multiplyIntSpace(ia, ib, uint8_map)
-    return ir / scale + min_f
+
+    if map_type == 'signed_log':
+        # Decode logarithmic mapping
+        fz_log = (ir / 127.5) - 1
+        sign = np.sign(fz_log)
+        abs_val = np.abs(fz_log)
+        fz = sign * ((10 ** abs_val - 1) / 9) * 4  # inverse of encoding
+        return fz
+    else:
+        # Linear decode
+        return ir / scale + min_f
